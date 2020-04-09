24848930e41cbc7dab48bb5657a1cde3b2e8db22
Dhava Gautama <62224294+dhava-stmkg@users.noreply.github.com>
2020-04-09 08:59:19 +0700

    Update 4x4.py

--- 4x4.py
+++ 4x4.py
@@ -1,3 +1,7 @@
+# Find solution of linear problem using gauss eliminition
+# Dhava Gautama
+# https://github.com/dhava-stmkg/Gauss_LinearSolver
+
 #matrik = [1,2,3,-1,10],[2,5,4,8,8],[4,2,2,1,-2],[6,4,-1,-2,4]
 matrik = [4,4,1,1,11],[2,1,2,-2,2],[1,-1,-1,2,0],[1,1,1,-1,2]
 
